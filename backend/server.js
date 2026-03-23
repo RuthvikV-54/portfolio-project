4a9b01900887ce59617df75cfd653d7b8d29ad54
Ruthvik-54 <25bcac54@kristujayanti.com>
2026-03-23 19:31:36 +0530

    Clean portfolio project

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// ✅ ADD HERE (right after middleware)
+mongoose.connect("mongodb://25bcac54_db_user:AVhVGWEqxTPsRHcs@ac-ppwqfdp-shard-00-00.awwemyr.mongodb.net:27017,ac-ppwqfdp-shard-00-01.awwemyr.mongodb.net:27017,ac-ppwqfdp-shard-00-02.awwemyr.mongodb.net:27017/?ssl=true&replicaSet=atlas-6d7eq5-shard-0&authSource=admin&appName=Cluster0")
+.then(() => console.log("✅ MongoDB Connected"))
+.catch(err => {
+  console.log("❌ FULL DB ERROR:");
+  console.log(err);
+});
+
+// Schema
+const Contact = mongoose.model("Contact", {
+  name: String,
+  email: String,
+  message: String
+});
+
+// Route
+app.post("/contact", async (req, res) => {
+  try {
+    const newMsg = new Contact(req.body);
+    await newMsg.save();
+    res.send("Message saved successfully!");
+  } catch (err) {
+    res.status(500).send("Error saving message");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
